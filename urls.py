3dc96e493f5f5f4b1993c49f414ed6a1683648df
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-06-15 03:20:36 +0300

    Added URLs for piechart data which is obtained by the flash chart on the page, URLs include - pie chart data - bar graph data - bar glass data - line graph data

--- urls.py
+++ urls.py
@@ -13,3 +13,13 @@ urlpatterns = patterns('',
     url(r'^$', 'core.views.dashboard', name="homepage"),
     (r'', include('cis.core.urls')),
 )
+# the chart data views
+urlpatterns += patterns('reports.views',
+    ('^data/pie/$','chart_data_pie'),
+    ('^data/bar/$','chart_data_bar'),
+    ('^data/barglass/$','chart_data_bar_glass'),
+    ('^data/line/$','chart_data_line'),
+)
+urlpatterns += patterns('',
+    (r'^charts/$', 'django.views.generic.simple.direct_to_template', {'template': 'reports/chart.html'}),
+)
